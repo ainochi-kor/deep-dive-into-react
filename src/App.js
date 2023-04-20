5f6deb1663783eb3807363757c79983b4fb33b03
ainochi-kor <kthrkdals@gmail.com>
2023-04-21 00:05:47 +0900

    add: about, profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import QuickStartPage from './page/quick-start';
+import About from './page/about';
+import Profile from './page/profile';
+
+const showQuickStart = () => {
+  if (window.location.pathname === "/") {
+    return <QuickStartPage />
+  }
+}
+
+const showAbout = () => {
+  if (window.location.pathname === "/about") {
+    return <About />
+  }
+}
+
+const showProfile = () => {
+  if (window.location.pathname === "/profile") {
+    return <Profile />
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <QuickStartPage />
+      {showQuickStart()}
+      {showAbout()}
+      {showProfile()}
     </div>
   );
 }
--- src/page/quick-start.jsx
+++ src/page/quick-start.jsx
@@ -3,7 +3,9 @@ import MyButton from "../components/MyButton";
 const QuickStartPage = () => {
   return (
     <div>
+      <h1>Welcome to my app</h1>
       <MyButton />
+      <img className="avatar" />
     </div>
   );
 };
